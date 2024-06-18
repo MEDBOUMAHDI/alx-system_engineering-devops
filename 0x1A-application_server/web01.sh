2f09975bb82420f288cb4d52807df7bfc5eefef9
medboumahdi <med.boumahdi.med.com>
2024-06-18 03:16:56 -0700

    0x1A. Application server

--- 0x1A-application_server/web01.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Script to start the gunicorn server serving task 0 of web_flask
-gunicorn --bind 0.0.0.0:5000 web_flask.0-hello_route:app
-
--- 0x1A-application_server/web_flask/__init__.py
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/python3
-"""The web_falsk module home file"""
-
